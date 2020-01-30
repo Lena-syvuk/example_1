395a405f27ec55bcfbf9428bd9b2353ac6657a13
Lena-syvuk <syvykolena@gmail.com>
2020-01-30 10:52:13 +0200

    first commit

---
+++ if.py
@@ -0,0 +1,19 @@
+print('Введите A:')
+a = int(input())
+print('Введите B:')
+b = int(input())
+print('Введите C')
+c = int(input())
+z = int(a) + int(c)
+q = int(b) - int(c)
+if a > b:
+    print('Совершилось')
+elif b > a:
+    print(' Да ну')
+elif a == b:
+    print('А если так')
+elif z > c:
+    print('Совершилось')
+elif z < c:
+    print(' Да ну')
+    
